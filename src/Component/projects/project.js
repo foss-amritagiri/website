b251dae9a10795d1bb357f8854c2e6658c3653c3
kryptocodes <srivatsantb@gmail.com>
2020-04-23 20:07:37 +0530

    Added projects
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- src/Component/projects/project.js
+++ src/Component/projects/project.js
@@ -1,30 +1,35 @@
 import React from 'react';
 import Nav from '../nav/nav';
 import Footer from '../home/footer'
+import FlipCard from "react-flip-cards";
+import Team from '../../images/team.svg'
+import 'react-flip-cards/build/index.css'
 
-const project = () => (
-    <div className="page-container">
-    <Nav/>
-    <div className="container-fluid py-1">
-            <div className="jumbotron bg-dark text-white text-center">
-                <h2 className="display-4">Hello world</h2>
-                <p className="lead">this is project page</p>
-            </div>
-            <div className="jumbotron bg-dark text-white text-center">
-                <h2 className="display-4">Hello world</h2>
-                <p className="lead">this is project page</p>
-            </div>
-            <div className="jumbotron bg-dark text-white text-center">
-                <h2 className="display-4">Hello world</h2>
-                <p className="lead">this is project page</p>
-            </div>
-            <div className="jumbotron bg-dark text-white text-center">
-                <h2 className="display-4">Hello world</h2>
-                <p className="lead">this is project page</p>
-            </div>
-     </div>
-     <Footer/>
-     </div>
+const Card1 = () => (
+    <FlipCard 
+        fontTitle="Amrita Repo"
+        fontSubTitle="web application"
+        backTitle="Amrita Repo"
+        backText="A simple webapp for amritian"
+        >
+        <a href="https://github.com/">View on Github</a>
+        </FlipCard>
+
 )
 
-export default project
+export default function project() {
+    return (
+        <div className="page-container">
+        <Nav/>
+        <h1 className="text-center">Projects</h1>
+        <div className="container py-1">
+            <div className="row">
+                <div className="col-md-4">
+                <Card1/>
+                </div>
+            </div>
+        </div>
+        <Footer/>
+        </div>
+    )
+}
