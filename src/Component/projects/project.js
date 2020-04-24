8fc49f744795365ca1d11568e5f2d284471e2172
kryptocodes <srivatsantb@gmail.com>
2020-04-24 11:34:32 +0530

    Added project page
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- src/Component/projects/project.js
+++ src/Component/projects/project.js
@@ -2,8 +2,8 @@ import React from 'react';
 import Nav from '../nav/nav';
 import Footer from '../home/footer'
 import FlipCard from "react-flip-cards";
-import Team from '../../images/team.svg'
 import 'react-flip-cards/build/index.css'
+import './project.css'
 
 const Card1 = () => (
     <FlipCard 
@@ -17,15 +17,33 @@ const Card1 = () => (
 
 )
 
+const Card2 = () => (
+    <FlipCard 
+        fontTitle="GPMS"
+        fontSubTitle="Gate pass management system"
+        backTitle="GPMS"
+        backText="Gate pass management system"
+        >
+        <a href="https://github.com/kryptocodes/GPMS_backend">View on Github</a>
+        </FlipCard>
+
+)
+
 export default function project() {
     return (
         <div className="page-container">
         <Nav/>
-        <h1 className="text-center">Projects</h1>
-        <div className="container py-1">
-            <div className="row">
-                <div className="col-md-4">
+        <div className="jumbotron">
+        <h1 className="display-4 text-center text-uppercase">Projects</h1>
+        <p className="lead text-center py-1">A curated collection of open source projects which are contributed and maintained by foss members</p>
+        </div>
+        <div className="container">
+            <div className="row p-1">
+                <div className="col-9 col-md-4 mx-auto p-2">
                 <Card1/>
+                </div>
+                <div className="col-9 col-md-4 mx-auto p-2">
+                <Card2/>
                 </div>
             </div>
         </div>
